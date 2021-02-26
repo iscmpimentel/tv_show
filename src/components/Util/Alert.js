87344d1e9a3e8cd038a08ed41ebe5f56a4234fd5
iscmpimentel <isc.mpimentel@gmail.com>
2021-02-26 15:35:29 -0600

    third part

--- src/components/Util/Alert.js
+++ src/components/Util/Alert.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types'
 const Alert = (props) => {
     return(
         <div className="div_alert div_alert_danger  ">
-            <p className="text-center"><strong > {props.message}</strong > Hola mundo as second part</p>
+            <p className="text-center"><strong > {props.message}</strong > Hola mundo as second part as third part</p>
         </div>
     );
 }
